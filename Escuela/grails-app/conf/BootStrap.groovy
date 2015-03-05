7b3bf85d1ac11c4b022188df9f8886977289c939
evillanueva <evillanueva@shinji>
2015-03-05 16:52:55 -0600

    Resolviendo mis conflictos

--- Escuela/grails-app/conf/BootStrap.groovy
+++ Escuela/grails-app/conf/BootStrap.groovy
@@ -3,9 +3,6 @@ class BootStrap {
     def init = { servletContext ->
         //CABIOS DE PRUEBA
         // BLA BLBA
-<<<<<<< HEAD
-        //Hola
-=======
         // BLA BLBA
         // BLA BLBA
         // BLA BLBA
@@ -18,7 +15,6 @@ class BootStrap {
         // BLA BLBA
         // BLA BLBA
         // BLA BLBA
->>>>>>> e7606be1a8a42bebb13612495367e7b10ec17604
     }
     def destroy = {
     	//mas comentaarios	
