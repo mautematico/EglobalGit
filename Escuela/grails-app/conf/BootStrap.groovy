e7606be1a8a42bebb13612495367e7b10ec17604
evillanueva <evillanueva@shinji>
2015-03-05 16:30:12 -0600

    CAMBIOS 2222

--- Escuela/grails-app/conf/BootStrap.groovy
+++ Escuela/grails-app/conf/BootStrap.groovy
@@ -3,6 +3,18 @@ class BootStrap {
     def init = { servletContext ->
         //CABIOS DE PRUEBA
         // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
+        // BLA BLBA
     }
     def destroy = {
     }
