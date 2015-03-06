6ca8eae02822488cd35109bb81925da7e7a529a5
evillanueva <evillanueva@shinji>
2015-03-06 11:54:22 -0600

    Mi cambio fue primero

--- Escuela/grails-app/conf/BootStrap.groovy
+++ Escuela/grails-app/conf/BootStrap.groovy
@@ -16,7 +16,7 @@ class BootStrap {
         // BLA BLBA
         // BLA BLBA
         //comentarios de prueba
-        //segundo comentario de prueba
+        //segundo comentario de prueba EDER VILLANUEVA CORTEZ
     }
     def destroy = {
     	//mas comentaarios	
