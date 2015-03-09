4912d6d47baf367c28c2b03d52114b4326ce42c0
evillanueva <evillanueva@shinji>
2015-03-09 12:41:45 -0600

    Agregue autores

--- Biblioteca/grails-app/conf/BootStrap.groovy
+++ Biblioteca/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,22 @@
+import com.mx.eglobal.Autor
+import com.mx.eglobal.Books
 class BootStrap {
 
     def init = { servletContext ->
+    	if(!Autor.count()){
+    		new Autor(nombre:"Antonio", appat:"Castom", apmat:"Blancas").save(failOnError:true);
+    		new Autor(nombre:"Bianca", appat:"Matias",  apmat:"Herrera").save(failOnError:true);
+    		new Autor(nombre:"Carlos", appat:"Lopez",  apmat:"Aguilar").save(failOnError:true);
+    		new Autor(nombre:"Daniel", appat:"Martinez", apmat:"Hernandez").save(failOnError:true);
+    		new Autor(nombre:"Edgar", appat:"Pahua", apmat:"Ibarra").save(failOnError:true);
+    		new Autor(nombre:"Carlos", appat:"Lopez",  apmat:"Aguilar").save(failOnError:true);
+    		new Autor(nombre:"Daniel", appat:"Martinez", apmat:"Hernandez").save(failOnError:true);
+    		new Autor(nombre:"Edgar", appat:"Pahua", apmat:"Ibarra").save(failOnError:true);
+    		new Autor(nombre:"Carlos", appat:"Lopez",  apmat:"Aguilar").save(failOnError:true);
+    		new Autor(nombre:"Daniel", appat:"Martinez", apmat:"Hernandez").save(failOnError:true);
+    		new Autor(nombre:"Edgar", appat:"Pahua", apmat:"Ibarra").save(failOnError:true);
+    		
+    	}
     }
     def destroy = {
     }
