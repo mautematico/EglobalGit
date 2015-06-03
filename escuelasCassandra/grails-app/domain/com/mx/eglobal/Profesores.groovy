414ce1f84690da73170ee95fd93e043076e039e4
JessicaMatiasEG <iscmatias@outlook.com>
2015-06-03 14:10:50 -0500

    agregue variables a profesores

--- escuelasCassandra/grails-app/domain/com/mx/eglobal/Profesores.groovy
+++ escuelasCassandra/grails-app/domain/com/mx/eglobal/Profesores.groovy
@@ -1,7 +1,20 @@
 package com.mx.eglobal
 
 class Profesores {
+	int empleado;
+	String nombre;
+	String apepat;
+	String apemat;
+
 
     static constraints = {
     }
+    static mapping = {
+    	    table "profesores"
+        		version false
+	            empleado column:"empleado"
+	            nombre column: "nombre"
+	            apepat column: "apepat"
+	            apemat column: "apemat"
+        }
 }
