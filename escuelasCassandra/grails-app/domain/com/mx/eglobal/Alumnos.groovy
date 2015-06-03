9e9c134af0c0f063ccc4c68b762d6b15d896c98c
mautematico <mauricio@navarromiranda.mx>
2015-06-03 17:36:49 -0500

    Alumnos.matricula -> Alumnos.id

--- escuelasCassandra/grails-app/domain/com/mx/eglobal/Alumnos.groovy
+++ escuelasCassandra/grails-app/domain/com/mx/eglobal/Alumnos.groovy
@@ -1,24 +1,21 @@
 package com.mx.eglobal
 
 class Alumnos {
-//	Integer id;
+	Integer id;
 	String nombre;
 	String apellido;
 	String escuela;
 	String grupo;
-	Integer matricula
-
 
     static constraints = {
     }
     static mapping = {
     	    table "alumnos"
         		 version false
-//	            id column: "id"
+	            id column: "id"
 	            nombre column: "nombre"
 	            apellido column: "apellido"
 	            escuela column: "escuela"
 	            grupo column: "grupo"
-	            matricula column: "id"
         }
 }
