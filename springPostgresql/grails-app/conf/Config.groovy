015506e5459e8ea4b84bbfafcd4b492580d63e70
mautematico <mauricio@navarromiranda.mx>
2015-06-09 15:23:27 -0500

    s2-quickstart

--- springPostgresql/grails-app/conf/Config.groovy
+++ springPostgresql/grails-app/conf/Config.groovy
@@ -115,3 +115,20 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.mx.eglobal.Usuario'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.mx.eglobal.UsuarioRol'
+grails.plugin.springsecurity.authority.className = 'com.mx.eglobal.Rol'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['permitAll'],
+	'/index':           ['permitAll'],
+	'/index.gsp':       ['permitAll'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll']
+]
+
