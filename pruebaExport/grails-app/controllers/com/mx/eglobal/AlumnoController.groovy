58370a0df8a22e28e5ecbdf84daf6800b35fe965
mautematico <mauricio@navarromiranda.mx>
2015-06-10 17:06:03 -0500

    configuro el controller de Alumno

--- pruebaExport/grails-app/controllers/com/mx/eglobal/AlumnoController.groovy
+++ pruebaExport/grails-app/controllers/com/mx/eglobal/AlumnoController.groovy
@@ -4,14 +4,43 @@ package com.mx.eglobal
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
+import com.mx.eglobal.*;
+
 
 @Transactional(readOnly = true)
 class AlumnoController {
+    def exportService
+    def grailsApplication
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
+
+
+
+        println params;
+
+        if(params?.extension){
+            response.contentType = grailsApplication.config.grails.mime.types[params.extension]
+            response.setHeader("Content-disposition", "attachment; filename=books.${params.extension}")
+
+
+            List fields = ["nombre", "edad"]
+            Map labels = ["nombre": "Nombre", "edad": "Edad"]
+                        // Formatter closure
+            def upperCase = { domain, value ->
+                return value.toUpperCase()
+            }
+
+            Map formatters = [nombre: upperCase]        
+            Map parameters = [edad: "Edades", "column.widths": [0.2, 0.3, 0.5]]
+
+            exportService.export(params.extension, response.outputStream, Alumno.list(params), fields, labels, formatters, parameters)
+        }
+
+
+
         respond Alumno.list(params), model:[alumnoInstanceCount: Alumno.count()]
     }
 
