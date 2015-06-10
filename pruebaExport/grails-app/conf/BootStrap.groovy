bf64b4a8d1c7f4e2917fce149f264e2e87b69604
mautematico <mauricio@navarromiranda.mx>
2015-06-10 17:04:50 -0500

    Configuro el plugin y  creo algunos objetos de ejemplo

--- pruebaExport/grails-app/conf/BootStrap.groovy
+++ pruebaExport/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import com.mx.eglobal.Alumno
+
 class BootStrap {
 
     def init = { servletContext ->
+    	(1..20).each {
+    		new Alumno(nombre:"Ola", edad:it).save()
+    	}
+
     }
     def destroy = {
     }
